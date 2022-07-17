18d91ad949b657341a4fe8db53f92a06ee6b459a
Ashish Sharda <ashishjsharda@gmail.com>
2022-07-17 12:42:59 -0500

    Update message

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import './App.css';
 function App() {
   let isMorning=(new Date()).getHours()<12;
   let greetingElement=isMorning ? <h3>Good Morning </h3> :<h3> Good Evening  </h3>
+  let adjective='cool';
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         {greetingElement}
         <p>
-          This is so cool...
+          This is so {adjective}...
         </p>
         <a
           className="App-link"
